4718bd6a252dbc3b25dcf1c0c9a3dd7122bd2efc
Jordy Moos <jordymoos@gmail.com>
2019-01-03 18:06:32 +0100

    Create the add poll

--- lib/vocial_web/controllers/poll_controller.ex
+++ lib/vocial_web/controllers/poll_controller.ex
@@ -1,11 +1,25 @@
 defmodule VocialWeb.PollController do
   use VocialWeb, :controller
+  alias Vocial.Votes
 
   def index(conn, _params) do
-    polls = Vocial.Votes.list_polls()
+    polls = Votes.list_polls()
 
-    conn
-      |> put_layout(:special)
-      |> render "index.html", polls: polls
+    render conn, "index.html", polls: polls
+  end
+
+  def new(conn, _params) do
+    poll = Votes.new_poll()
+
+    render conn, "new.html", poll: poll
+  end
+
+  def create(conn, %{"poll" => poll_params, "options" => options}) do
+    split_options = String.split(options, ",")
+    with {:ok, poll} <- Votes.create_poll_with_options(poll_params, split_options) do
+      conn
+        |> put_flash(:info, "Poll created successfully!")
+        |> redirect(to: poll_path(conn, :index))
+    end
   end
 end
